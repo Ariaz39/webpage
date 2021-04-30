1f8cff7033c17e0e6b4d3ce96735d8b09248c9ca
Alejo Ariaz <ariaz39@hotmail.com>
2021-04-30 02:17:58 -0500

    Montaje de la información en el sitio web.

--- reporte_de_gastos/resources/views/principal/inicio.blade.php
+++ reporte_de_gastos/resources/views/principal/inicio.blade.php
@@ -891,25 +891,25 @@
                     <div class="footer-col last">
                         <h4>Redes Sociales</h4>
                         <span class="fa-stack">
-                            <a href="https://facebook.com">
+                            <a href="https://facebook.com" target="_blank">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-facebook-f fa-stack-1x"></i>
                             </a>
                         </span>
                         <span class="fa-stack">
-                            <a href="https://twitter.com">
+                            <a href="https://twitter.com" target="_blank">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-twitter fa-stack-1x"></i>
                             </a>
                         </span>
                         <span class="fa-stack">
-                            <a href="https://instagram.com">
+                            <a href="https://instagram.com" target="_blank">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-instagram fa-stack-1x"></i>
                             </a>
                         </span>
                         <span class="fa-stack">
-                            <a href="https://linkedin.com">
+                            <a href="https://linkedin.com" target="_blank">
                                 <i class="fas fa-circle fa-stack-2x"></i>
                                 <i class="fab fa-linkedin-in fa-stack-1x"></i>
                             </a>
